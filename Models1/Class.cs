86a8ba0a4a84f2623e9cbbfa70027c5f740f2616
Jonaslk727 <joloknu@gmail.com>
2025-11-04 11:35:12 +0100

    trek database tables ned som modelklasser

---
+++ Models1/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2._Sem_Project_Eksamen_System.Models1;
+
+[Table("Class")]
+public partial class Class
+{
+    [Key]
+    [Column("ClassID")]
+    public int ClassId { get; set; }
+
+    [StringLength(250)]
+    [Unicode(false)]
+    public string ClassName { get; set; } = null!;
+
+    [InverseProperty("Class")]
+    public virtual ICollection<Exam> Exams { get; set; } = new List<Exam>();
+
+    [InverseProperty("Class")]
+    public virtual ICollection<StudentsToClass> StudentsToClasses { get; set; } = new List<StudentsToClass>();
+}
---
+++ Models1/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2._Sem_Project_Eksamen_System.Models1;
+
+[Table("Room")]
+public partial class Room
+{
+    [Key]
+    [Column("RoomID")]
+    public int RoomId { get; set; }
+
+    [StringLength(250)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    public int? Capacity { get; set; }
+
+    [InverseProperty("Room")]
+    public virtual ICollection<RoomsToExam> RoomsToExams { get; set; } = new List<RoomsToExam>();
+}
