5b153bc7c8b13333c1ca2439e1d3974e038ec857
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2025-11-05 10:03:12 +0100

    Student

---
+++ Pages/Eksamner/Students/Student.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _2._Sem_Project_Eksamen_System.Pages.Eksamner.Students
+{
+    public class StudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
