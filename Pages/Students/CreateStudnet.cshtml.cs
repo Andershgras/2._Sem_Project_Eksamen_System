37e36cfbd712270faaa913e7a545ffc36b1b8a95
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2025-11-05 10:37:48 +0100

    CreateS

---
+++ Pages/Students/CreateStudnet.cshtml.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using _2._Sem_Project_Eksamen_System.Interfaces;
+using _2._Sem_Project_Eksamen_System.Models1;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _2._Sem_Project_Eksamen_System.Pages.Eksamner.Students
+{
+    public class CreateStudentModel : PageModel
+    {
+        private readonly ICRUDT<Student> _service;
+
+        [BindProperty]
+        public Student Student { get; set; } = new Student();
+
+        public CreateStudentModel(ICRUDT<Student> service)
+        {
+            _service = service;
+        }
+
+        public void OnGet()
+        {
+            // show empty form
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _service.AddItem(Student);
+            return RedirectToPage("/Eksamner/Students/GetStudent");
+        }
+    }
+}
