948d394eaeb2c09e4ef08837bcc8c52868f48f72
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2025-11-05 10:14:45 +0100

    lave getStudent

---
+++ Pages/Eksamner/Students/GetStudent.cshtml.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _2._Sem_Project_Eksamen_System.Interfaces;
+using _2._Sem_Project_Eksamen_System.Models1;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _2._Sem_Project_Eksamen_System.Pages.Eksamner.Students
+{
+    public class GetStudentModel : PageModel
+    {
+        private readonly ICRUDT<Student> _service;
+
+        public IEnumerable<Student> Students { get; private set; } = Enumerable.Empty<Student>();
+
+        [BindProperty(SupportsGet = true)]
+        public GenericFilter Filter { get; set; } = new GenericFilter();
+
+        public GetStudentModel(ICRUDT<Student> service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Students = await _service.GetAll(Filter);
+            if (Students == null)
+            {
+                Students = Enumerable.Empty<Student>();
+            }
+
+            return Page();
+        }
+    }
+}
