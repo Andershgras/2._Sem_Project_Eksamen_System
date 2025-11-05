185aa01420acf0248659f124533ec78cff9e5242
Andershgras <andershgras@gmail.com>
2025-11-05 09:48:26 +0100

    - Added Search functionality to Rooms (only name)

--- Pages/Rooms/Index.cshtml.cs
+++ Pages/Rooms/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using _2._Sem_Project_Eksamen_System.Interfaces;
 using _2._Sem_Project_Eksamen_System.Models1;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace _2._Sem_Project_Eksamen_System.Pages.Rooms
@@ -8,9 +9,20 @@ namespace _2._Sem_Project_Eksamen_System.Pages.Rooms
     {
         private readonly ICRUD<Room> _service;
         public IEnumerable<Room> Rooms { get; set; } = Enumerable.Empty<Room>();
-
+        [BindProperty(SupportsGet = true)]
+        public GenericFilter? Filter { get; set; }
         public IndexModel(ICRUD<Room> service) => _service = service;
-
-        public void OnGet() => Rooms = _service.GetAll();
+        public void OnGet()
+        {
+            if (Filter is not null && !string.IsNullOrWhiteSpace(Filter.FilterByName)
+                && _service is _2._Sem_Project_Eksamen_System.EFservices.EFRoomService rooms)
+            {
+                Rooms = rooms.Search(Filter!);
+            }
+            else
+            {
+                Rooms = _service.GetAll();
+            }
+        }
     }
 }
