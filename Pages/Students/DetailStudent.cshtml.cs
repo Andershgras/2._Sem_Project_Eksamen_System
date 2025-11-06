5d47b2a3099ae0b0ec7c9e9328fcab1e8bca7c13
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2025-11-06 10:05:13 +0100

    check

--- Pages/Students/DetailStudent.cshtml.cs
+++ Pages/Students/DetailStudent.cshtml.cs
@@ -1,12 +1,33 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using _2._Sem_Project_Eksamen_System.Interfaces;
+using _2._Sem_Project_Eksamen_System.Models1;
 
 namespace _2._Sem_Project_Eksamen_System.Pages.Students
 {
     public class DetailStudentModel : PageModel
     {
-        public void OnGet()
+        private readonly ICRUDT<Student> _studentService;
+
+        public Student Student { get; set; } = new Student();
+
+        public DetailStudentModel(ICRUDT<Student> studentService)
         {
+            _studentService = studentService;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var student = await _studentService.GetItemById(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            Student = student;
+            return Page();
         }
     }
 }
