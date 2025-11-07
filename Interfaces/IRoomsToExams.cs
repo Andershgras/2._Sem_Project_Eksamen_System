6329329bc96292aaa55020f503692b40e8aa1bc7
Jonaslk727 <joloknu@gmail.com>
2025-11-07 11:03:38 +0100

    added interface

---
+++ Interfaces/IRoomsToExams.cs
@@ -0,0 +1,11 @@
+﻿using _2._Sem_Project_Eksamen_System.Models1;
+
+namespace _2._Sem_Project_Eksamen_System.Interfaces
+{
+    public interface IRoomsToExams : ICRUDT<RoomsToExam>
+    {
+        void AddTeachersFromClassToExam(int roomId, int e);
+
+      
+    }
+}
