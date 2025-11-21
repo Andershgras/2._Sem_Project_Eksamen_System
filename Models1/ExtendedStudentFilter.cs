9a2764ab7deca800746bfcd4cd55d744e4e713f8
Jonaslk727 <joloknu@gmail.com>
2025-11-21 09:38:33 +0100

    Nu omskrives -id til id i søgning af students

--- Models1/ExtendedStudentFilter.cs
+++ Models1/ExtendedStudentFilter.cs
@@ -8,8 +8,18 @@ namespace _2._Sem_Project_Eksamen_System.Models1
 {
     public class ExtendedStudentFilter : GenericFilter
     {
+        private int? _filterById = null;
         public string FilterByEmail { get; set; } = string.Empty;
         public string FilterByClass { get; set; } = string.Empty;
-        public int? FilterById { get; set; }
+        
+        public int? FilterById
+        {
+            get { return _filterById; }
+            set { if (value < 0) _filterById = value * -1;
+                    else _filterById = value;
+            }
+        }
+
+        
     }
 }
